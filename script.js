13f88eea7bdebcdf3f2dfb0c02070a428dd6da6a
Kavya Parmar <kavyaparmar203@gmail.com>
2025-06-13 22:59:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log("Welcome to Clone");
+
+//Initialize the variables
+let songIndex=0;
+let audioElement=new Audio('songs/1.mp3');
+let masterPlay=document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('myProgressBar');
+let gif=document.getElementById('gif');
+let masterSongName=document.getElementById('masterSongName');
+let songItems=Array.from(document.getElementsByClassName('songItem'));
+
+let songs=[
+    {songName:"Let me Love You",filepath:"songs/1.mp3", coverPath:"covers/10.jpg"},
+    {songName:"Life Goes On",filepath:"songs/2.mp3", coverPath:"covers/8.jpg"},
+    {songName:"Pump It",filepath:"songs/3.mp3", coverPath:"covers/7.jpg"},
+    {songName:"Senorita",filepath:"songs/4.mp3", coverPath:"covers/6.jpg"},
+    {songName:"In the Mist",filepath:"songs/5.mp3", coverPath:"covers/5.jpg"},
+    {songName:"Me & Devil",filepath:"songs/6.mp3", coverPath:"covers/4.jpg"},
+    {songName:"Dolittle",filepath:"songs/7.mp3", coverPath:"covers/3.jpg"},
+    {songName:"Baby",filepath:"songs/8.mp3", coverPath:"covers/2.jpg"},
+    {songName:"I'm still standing",filepath:"songs/9.mp3", coverPath:"covers/1.jpg"},
+    {songName:"I Don't Care",filepath:"songs/10.mp3", coverPath:"covers/9.jpg"},
+]
+
+//audioElement.play();
+
+//Handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity=1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity=0;
+    }
+})
+
+//Listen to events
+document.addEventListener('timeupdate',()=>{
+    console.log("Time update");
+})
